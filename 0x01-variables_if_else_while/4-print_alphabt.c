5c4cc89f78193b9052b7fcd1539cc9e2e6a75f5f
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-17 12:47:08 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-/**
- * main - Entry point
- * Return: Always 0 (success)
- */
 int main(void)
 {
-	int n;
+    char letter;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e')
+        {
+            putchar(letter);
+        }
+    }
+    putchar('\n');
+    return 0;
 }
