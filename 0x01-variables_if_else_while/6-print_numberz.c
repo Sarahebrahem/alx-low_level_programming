d6f78ba953465b173ed221e7b9aef9712c5e1ebf
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-17 13:01:47 -0700

    Create 6-print_numberz.c file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry  point
+ * main - Entry point
  * Return: 0 (success)
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i % 10 + 48);
 }
+putchar('\n');
+return (0);
+}
+
