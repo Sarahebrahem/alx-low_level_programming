abf963a72cb3c5830a5c0c8361136a946d507aa4
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-17 13:12:26 -0700

    Create 8-print_base16 file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  * Return: 0 (success)
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+for (digit = 'a'; digit <= 'f'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
 }
