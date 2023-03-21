5f968a8bc2211cb17dda86fe8cc689004730c28a
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-21 11:42:33 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,25 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 /**
- * print_sign - function that prints the sign of a number.
- * @n: the int to be checked
- * Return: 1 for postive num, -1 for negative num and 0 otherwise.
+ * print_sign -  function that prints the sign of a number
+ * @n: the int will be checked
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 otherwise.
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
-putchar(43);
+printf("+");
 return (1);
 }
-else if (n < 0)
+else if (n == 0)
 {
-putchar(45);
-return (-1);
+printf("0");
+return (0);
 }
 else
 {
-putchar(48);
-return (0);
+printf("-");
+return (-1);
 }
 }
