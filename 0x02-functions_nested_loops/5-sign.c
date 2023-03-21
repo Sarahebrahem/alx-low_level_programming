fc65815d4e9868a0e05ca8772090a50b9d3c988c
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-21 10:51:33 -0700

    Create 5-sign.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,23 @@
 /**
  * print_sign - function that prints the sign of a number.
  * @n: the int to be checked
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 for postive num, -1 for negative num and 0 otherwise.
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+putchar(43);
 return (1);
 }
-else if (n == 0)
+else if (n < 0)
+{
+putchar(45);
+return (-1);
+}
+else
+{
+putchar(48);
+return (0);
+}
 }
