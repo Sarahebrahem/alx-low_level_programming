e6474c3d359526fc4ea534160a513e74d8201ea8
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-23 17:17:27 -0700

    Create 10-print_triangle.c file

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,24 +7,18 @@
  */
 void print_triangle(int size)
 {
+int i, j;
 if (size <= 0)
 {
 _putchar('\n');
+return;
 }
-else
-{
-int i, j;
 for (i = 1; i <= size; i++)
 {
-for (j = i; j < size; j++)
-{
-_putchar('_');
-}
-for (i = 1; j <= i; j++)
+for (j = 1; j <= i; j++)
 {
 _putchar('#');
 }
 _putchar('\n');
-}
 }
 }
