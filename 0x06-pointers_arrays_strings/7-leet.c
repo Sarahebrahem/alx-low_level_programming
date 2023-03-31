10108f45908f2f86a67690e288c69acd6688e132
Sarahebrahem <sarahebrahem@outlook.com>
2023-03-31 13:11:31 -0700

    Create 7-leet.c file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: encoded string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; letters[j]; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
