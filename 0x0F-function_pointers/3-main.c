8fe7b0dc53e5d26a17dc0b3726d195117af9b57e
Sarahebrahem <sarahebrahem@outlook.com>
2023-04-20 18:18:01 -0700

    Edit 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+#include <string.h>
 
 /**
  * main - performs simple operations.
@@ -21,22 +22,26 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
 
-	if (op_func == NULL || argv[2][1] != '\0')
+	if (strchr("+-*/%", argv[2][0]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	result = op_func(num1, num2);
 	printf("%d\n", result);
-
 	return (0);
 }
