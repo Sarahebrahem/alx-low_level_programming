05e03f363803cf7aaf3cf2cc0ba9edd79089a480
Sarahebrahem <sarahebrahem@outlook.com>
2023-04-28 15:57:40 -0700

    Create 0-print_list.c file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * pre_main - Prints a message before main is executed.
+ * Return: void
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
