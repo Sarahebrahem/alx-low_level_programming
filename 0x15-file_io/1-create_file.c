2424caaebb5c79268851f166ed3902bd1d20113b
Sarahebrahem <sarahebrahem@outlook.com>
2023-05-08 12:25:05 -0700

    Edit 1-create_file.c file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,31 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 /**
- * create_file - creates a file with given content
+ * create_file - creates a file with the specified name and writes the
+ * specified text content to it.
  * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file
+ * @text_content: the content to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, flags, mode;
-	size_t len;
+	int fd, w, len;
 
 	if (filename == NULL)
 		return (-1);
-	flags = O_WRONLY | O_CREAT | O_TRUNC;
-	mode = S_IRUSR | S_IWUSR;
-	fd = open(filename, flags, mode);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
-			;
-		if (write(fd, text_content, len) != (ssize_t) len)
+		len = strlen(text_content);
+		w = write(fd, text_content, len);
+		if (w != len)
 		{
 			close(fd);
 			return (-1);
